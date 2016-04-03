2542610b520442fdd0d2f531bedbe83dd6bf5110
calpeyser <calpeyser@gmail.com>
2016-04-02 22:16:42 -0400

    update test script

--- test/simple_test.py
+++ test/simple_test.py
@@ -1,7 +1,20 @@
 import requests
 import json
+import sys
 
-params = json.dumps({'chorale': open('parallel_unison.xml', 'rb').read()})
-r = requests.post('http://192.168.99.101:8080/check', params)
+REMOTE_IP = "104.196.109.169"
+LOCAL_IP = "192.168.99.100"
 
-print r.json()
+def run_test(ip_address):
+	params = json.dumps({'chorale': open('parallel_unison.xml', 'rb').read()})
+	r = requests.post('http://%s:8080/check' % ip_address, params)
+
+	print r.json()
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		run_test(LOCAL_IP)
+	elif sys.argv[1] != "remote":
+		run_test(LOCAL_IP)
+	else:
+		run_test(REMOTE_IP)
